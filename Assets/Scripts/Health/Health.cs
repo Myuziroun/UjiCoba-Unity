af223c5e02ca486d4c0fa00c2c79f3eabffd3669
Myuziroun <alifrajafatahillah15@gmail.com>
2024-05-31 11:15:20 +0700

    penambahan logic health (unfinished)

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 
 public class Health : MonoBehaviour
-{   [SerializeField] private float startingHealth;
-    private float currentHealth;
+{   
+    [SerializeField] private float startingHealth;
+    public float currentHealth { get; private set; }
+   private Animator animasi;
 
     private void Awake()
     {
         currentHealth = startingHealth;
+        animasi = GetComponent<Animator>();
     }
     private void takeDamage(float _damage){
         currentHealth = Mathf.Clamp(currentHealth - _damage, 0 , startingHealth);
@@ -19,6 +22,12 @@ public class Health : MonoBehaviour
         }
     }
     
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)){
+            takeDamage(1);
+        }
+    }
 
     
 }
