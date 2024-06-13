04e216b5988db479500b97f0f67a14fb5753f8df
Myuziroun <alifrajafatahillah15@gmail.com>
2024-06-13 13:27:48 +0700

    penamabhan logic back ke main menu dengan menggunakn escape

--- Assets/Scripts/Chapter/BackLogic.cs
+++ Assets/Scripts/Chapter/BackLogic.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 public class BackLogic : MonoBehaviour
 {
     [SerializeField] private AudioClip clickTable;
-    public void backToMainMenu() {
+
+    private void Update()
+    {
+        backToMainMenuEscape();
+    }
+    public void backToMainMenuEscape() {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            SceneManager.LoadScene(0);
+        }
         // SoundManager.instance.PlaySound(clickTable);
+    }
+
+    public void backToMainMenuClick(){
         SceneManager.LoadScene(0);
     }
 }
