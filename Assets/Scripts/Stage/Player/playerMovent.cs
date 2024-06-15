ba3e1e35442e79d9c1702b75d5c0718c646e2c0c
Myuziroun <alifrajafatahillah15@gmail.com>
2024-06-15 11:47:26 +0700

    pembuatan logic untuk ke pindah ke stage dan logic untuk melompat pada player

--- Assets/Scripts/Stage/Player/playerMovent.cs
+++ Assets/Scripts/Stage/Player/playerMovent.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 public class playerMovent : MonoBehaviour
 {
     private float speed = 5;
+    private float jumpPower = 6;
     private float moveHorizontal;
     private float moveVertikal;
     private Rigidbody2D badan;
+    private Animator animasi;
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask groundLayer;
+    private float wallJumpCooldown;
 
     private  void Awake()
     {
         badan = GetComponent<Rigidbody2D>();
+        animasi = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
     private void Update()
     {
@@ -25,5 +32,31 @@ public class playerMovent : MonoBehaviour
         }else if(moveHorizontal < -0.01f){
             transform.localScale = new Vector3(-1, 1, 1);
         }
+
+        // animasi bergerak
+        animasi.SetBool("walk", moveHorizontal != 0);
+        // animasi pada waktu jatuh?
+        animasi.SetBool("grounded", isGrounded());
+        // logic lompat
+        if(wallJumpCooldown < 0.2f){
+            if(Input.GetKey(KeyCode.Space)){
+                playerLompat();            
+            }
+        }else{
+            wallJumpCooldown += Time.deltaTime;
+        }
+
+    }
+    private void playerLompat(){
+        if(isGrounded()){
+            badan.velocity = new Vector2(badan.velocity.x, jumpPower);
+            animasi.SetTrigger("jump");
+        }
+        // wallJumpCooldown += Time.deltaTime;
+    }
+
+    private bool isGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null; 
     }
 }
